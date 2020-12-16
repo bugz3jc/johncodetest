8f9c107aa75a6445c9a21b3d3146098b6b17611a
John Cris Tayco <bugz3jc@gmail.com>
2020-12-17 01:55:42 +0900

    task a with extra i and ii

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,32 @@
 // Put your application javascript here
+
+
+$(document).ready(function(){
+    $('#AddToCartForm').submit(function(e){
+        e.preventDefault();
+        var button = $(this).find('button#AddToCart');
+        var btnText = button.html();
+        var url = $(this).attr('action');
+        button.html("Adding to Cart...");
+        button.prop('disabled', true);
+
+        url = url.indexOf('.js') < 0 ? url + '.js' : url;
+
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function(data){
+                alert('Item added to cart.'); 
+                button.html(btnText);
+                button.prop('disabled', false);
+            },
+            error: function(data){
+                alert(data.responseJSON.description);
+                button.html(btnText);
+                button.prop('disabled', false);
+            }
+        });
+    });
+});
